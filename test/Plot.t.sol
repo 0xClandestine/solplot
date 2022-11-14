69ba2fcdbf78904c288dc1c51c1d4374e823c48b
0xClandestine <clandestine.eth@proton.me>
2022-11-13 23:39:19 -0600

    ♻️simplified demo

--- test/Plot.t.sol
+++ test/Plot.t.sol
@@ -23,21 +23,12 @@ contract DemoPlot is Plot {
         for (uint256 i; i < 100; i++) {
             // Use first row as legend
             if (i == 0) {
-                string[] memory cols = new string[](7);
-                
-                cols[0] = "x axis";
-                cols[1] = "h = 5";
-                cols[2] = "h = 10";
-                cols[3] = "h = 20";
-                cols[4] = "h = 30";
-                cols[5] = "h = 40";
-                cols[6] = "h = 50";
-
-                writeRowToCSV("input.csv", cols);
+                // Make sure the same amount of columns are included for the legend
+                vm.writeLine("input.csv", "x axis,h = 5,h = 10,h = 20,h = 30,h = 40,h = 50,");
             } else {
                 uint256[] memory cols = new uint256[](7);
 
-                cols[0] = i * 1e18 + 1e18;
+                cols[0] = i * 1e18;
                 cols[1] = expToTarget(1e18, 0.9e18, i - 1, 5);
                 cols[2] = expToTarget(1e18, 0.9e18, i - 1, 10);
                 cols[3] = expToTarget(1e18, 0.9e18, i - 1, 20);
