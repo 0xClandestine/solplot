83e4b8a0ec8fc9fdbe2207e46e7fbb9d018346d7
0xClandestine <clandestine.eth@proton.me>
2024-01-01 23:57:42 -0600

    fix: remove loop conditional

--- src/Plot.sol
+++ src/Plot.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
+/// @title Plot contract for generating plots from CSV data
+/// @dev This contract provides functions to generate plots and write rows to CSV files.
 abstract contract Plot is Test {
-    /// @notice Plot chart via plotly-rs + hevm ffi.
-    /// @param inputCsv - Path to csv file that contains the plot data.
-    /// @param outputSvg - Path to save svg output to.
-    /// @param inputDecimals - Denomination of the input data.
-    /// @param totalColumns - Total number of columns within inputCsv to plot.
-    /// @param legend - Determines whether the first row of the inputCsv should be used for legend naming.
+    /// @notice Generates a plot from input CSV data and saves it as an SVG file
+    /// @param inputCsv The input CSV data
+    /// @param outputSvg The output SVG file name
+    /// @param inputDecimals The number of decimals in the input data
+    /// @param totalColumns The total number of columns in the CSV data
+    /// @param legend A boolean indicating whether to include a legend in the plot
     function plot(
         string memory inputCsv,
         string memory outputSvg,
@@ -17,99 +19,99 @@ abstract contract Plot is Test {
         uint8 totalColumns,
         bool legend
     ) public {
-        if (legend) {
-            string[] memory ffi = new string[](10);
+        uint256 n;
 
-            ffi[0] = "solplot";
-
-            ffi[1] = "--input-file";
-            ffi[2] = inputCsv;
-
-            ffi[3] = "--output-file";
-            ffi[4] = outputSvg;
-
-            ffi[5] = "--decimals";
-            ffi[6] = vm.toString(inputDecimals);
-
-            ffi[7] = "--columns";
-            ffi[8] = vm.toString(totalColumns);
-
-            ffi[9] = "--legend";
-
-            vm.ffi(ffi);
-        } else {
-            string[] memory ffi = new string[](9);
-
-            ffi[0] = "solplot";
-
-            ffi[1] = "--input-file";
-            ffi[2] = inputCsv;
-
-            ffi[3] = "--output-file";
-            ffi[4] = outputSvg;
-
-            ffi[5] = "--decimals";
-            ffi[6] = vm.toString(inputDecimals);
-
-            ffi[7] = "--columns";
-            ffi[8] = vm.toString(totalColumns);
-
-            vm.ffi(ffi);
+        assembly {
+            n := add(9, legend)
         }
+
+        string[] memory ffi = new string[](n);
+
+        ffi[0] = "solplot";
+
+        ffi[1] = "--input-file";
+        ffi[2] = inputCsv;
+
+        ffi[3] = "--output-file";
+        ffi[4] = outputSvg;
+
+        ffi[5] = "--decimals";
+        ffi[6] = vm.toString(inputDecimals);
+
+        ffi[7] = "--columns";
+        ffi[8] = vm.toString(totalColumns);
+
+        if (legend) ffi[9] = "--legend";
+
+        vm.ffi(ffi);
     }
 
-    function writeRowToCSV(string memory file, string[] memory cols) public {
-        string memory row;
+    /// @notice Writes a row of string data to a CSV file
+    /// @param file The CSV file name
+    /// @param cols An array of string values representing the columns in the row
+    /// @return The name of the CSV file
+    function writeRowToCSV(string memory file, string[] memory cols)
+        public
+        returns (string memory)
+    {
+        unchecked {
+            uint256 n = cols.length;
 
-        uint256 length = cols.length;
-        for (uint256 i; i < length;) {
-            if (i == 0) {
-                row = string.concat(cols[i], ",");
-            } else {
+            string memory row = string.concat(cols[0], ",");
+
+            for (uint256 i = 1; i < n; ++i) {
                 row = string.concat(row, cols[i], ",");
             }
-            unchecked {
-                i += 1;
-            }
-        }
 
-        vm.writeLine(file, row);
+            vm.writeLine(file, row);
+
+            return file;
+        }
     }
 
-    function writeRowToCSV(string memory file, uint256[] memory cols) public {
-        string memory row;
+    /// @notice Writes a row of uint256 data to a CSV file
+    /// @param file The CSV file name
+    /// @param cols An array of uint256 values representing the columns in the row
+    /// @return The name of the CSV file
+    function writeRowToCSV(string memory file, uint256[] memory cols)
+        public
+        returns (string memory)
+    {
+        unchecked {
+            uint256 n = cols.length;
 
-        uint256 length = cols.length;
-        for (uint256 i; i < length;) {
-            if (i == 0) {
-                row = string.concat(vm.toString(cols[i]), ",");
-            } else {
+            string memory row = string.concat(vm.toString(cols[0]), ",");
+
+            for (uint256 i = 1; i < n; ++i) {
                 row = string.concat(row, vm.toString(cols[i]), ",");
             }
-            unchecked {
-                i += 1;
-            }
-        }
 
-        vm.writeLine(file, row);
+            vm.writeLine(file, row);
+
+            return file;
+        }
     }
 
+    /// @notice Writes a row of int256 data to a CSV file
+    /// @param file The CSV file name
+    /// @param cols An array of int256 values representing the columns in the row
+    /// @return The name of the CSV file
+    function writeRowToCSV(string memory file, int256[] memory cols)
+        public
+        returns (string memory)
+    {
+        unchecked {
+            uint256 n = cols.length;
 
-    function writeRowToCSV(string memory file, int256[] memory cols) public {
-        string memory row;
+            string memory row = string.concat(vm.toString(cols[0]), ",");
 
-        uint256 length = cols.length;
-        for (uint256 i; i < length;) {
-            if (i == 0) {
-                row = string.concat(vm.toString(cols[i]), ",");
-            } else {
+            for (uint256 i = 1; i < n; ++i) {
                 row = string.concat(row, vm.toString(cols[i]), ",");
             }
-            unchecked {
-                i += 1;
-            }
-        }
 
-        vm.writeLine(file, row);
+            vm.writeLine(file, row);
+
+            return file;
+        }
     }
 }
